2ca1e8ae7426f15649c68466a1a6dcf4f5625a40
Avenger767 <michaelhamoy@afgsim.com>
2026-04-10 17:25:16 -0500

    Create src/App.js for AFG Part Request App component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AfgPartRequestApp = () => {
+    return (
+        <div>
+            <h1>AFG Part Request App</h1>
+            <p>This is the AFG Part Request application.</p>
+        </div>
+    );
+};
+
+export default AfgPartRequestApp;
